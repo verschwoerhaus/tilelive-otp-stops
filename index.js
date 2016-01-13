c7a965db80a9ef28ad7d139b03b5ea7b1e04b327
Hannes Junnila <hannes.junnila@gmail.com>
2016-01-13 11:15:03 +0200

    Modify to read OTP index api

--- index.js
+++ index.js
@@ -3,26 +3,56 @@ const geojsonVt = require('geojson-vt');
 const vtPbf = require('vt-pbf');
 const url = require('url');
 const fs = require('fs');
+const request = require('requestretry');
+
+const query = `
+  query stops{
+    stops{
+      gtfsId
+      name
+      lat
+      lon
+      locationType
+      parentStation{
+        gtfsId
+      }
+      routes{
+        type
+      }
+    }
+  }`;
 
 class GeoJSONSource {
   constructor(uri, callback){
-    if (!uri.pathname) {
-      callback(new Error('Invalid directory ' + url.format(uri)));
-      return;
-    }
-
-    if (uri.hostname === '.' || uri.hostname == '..') {
-      uri.pathname = uri.hostname + uri.pathname;
-      delete uri.hostname;
-      delete uri.host;
-    }
-
-    fs.readFile(uri.pathname, function (err, data){
+    uri.protocol = "http:"
+    request({
+      url: uri,
+      body: query,
+      maxAttempts: 20,
+      retryDelay: 30000,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/graphql'
+      }
+    }, function (err, res, body){
       if (err){
+        console.log(err)
         callback(err);
         return;
       }
-      this.tileIndex = geojsonVt(JSON.parse(data), {maxZoom: 20}); //TODO: this should be configurable
+
+      const geoJSON = {type: "FeatureCollection", features: JSON.parse(body).data.stops.map(stop => ({
+        type: "Feature",
+        geometry: {type: "Point", coordinates: [stop.lon, stop.lat]},
+        properties: {
+          gtfsId: stop.gtfsId,
+          name: stop.name,
+          parentStation: stop.parentStation == null ? null : stop.parentStation.gtfsId,
+          type: stop.routes == null ? null : [...new Set(stop.routes.map(route => route.type))]
+        }
+      }))}
+
+      this.tileIndex = geojsonVt(geoJSON, {maxZoom: 20}); //TODO: this should be configurable
       callback(null, this)
     }.bind(this));
   };
@@ -47,5 +77,5 @@ class GeoJSONSource {
 module.exports = GeoJSONSource
 
 module.exports.registerProtocols = (tilelive) => {
-  tilelive.protocols['geojson:'] = GeoJSONSource
+  tilelive.protocols['otpstops:'] = GeoJSONSource
 }
